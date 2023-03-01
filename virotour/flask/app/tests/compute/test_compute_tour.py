432afb646d98c82c48c18bd35189ff123965ec63
Ivelin Tchangalov <i.tchangalov@gmail.com>
2023-03-01 14:38:03 -0500

    Add validations

--- virotour/flask/app/tests/compute/test_compute_tour.py
+++ virotour/flask/app/tests/compute/test_compute_tour.py
@@ -1,5 +1,6 @@
 from PIL import Image
-from app.tests.common_utils import add_tour, get_image_paths, upload_images, compute_tour, get_panoramic_image
+from app.tests.common_utils import add_tour, get_image_paths, upload_images, compute_tour, get_panoramic_image, \
+    get_raw_images
 
 
 def test_compute_tour(client):
@@ -10,8 +11,19 @@ def test_compute_tour(client):
 
     compute_tour(client, tour_name)
 
-    pano_image_path = get_panoramic_image(client, tour_name, 1)
+    pano_image_path = get_panoramic_image(client, tour_name, 1)['server_file_path']
     # pano_image_path = r"C:\Users\Ivo\Documents\spring2023\virotour\flask\uploads\panoramic_images\T_1_L_1_pano.png"
 
     img = Image.open(pano_image_path)
     # img.show()
+
+    # Validates that we can still retrieve the original images
+    data = get_raw_images(client, "Tour 1", 1)
+    assert data['count'] == 5
+    assert data['server_file_paths'] == [
+        'raw_images/T_1_L_1_S1.jpg',
+        'raw_images/T_1_L_1_S2.jpg',
+        'raw_images/T_1_L_1_S3.jpg',
+        'raw_images/T_1_L_1_S4.jpg',
+        'raw_images/T_1_L_1_S5.jpg'
+    ]
